44acef1040d0de28310a7c684921bc0a9df0a2d5
Deepak R C <deepakrchalmardbca25@rvu.edu.in>
2026-03-16 12:32:37 +0530

    Add files via upload

---
+++ singly linked list.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+void insertAtBeginning(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+    printf("Inserted %d at the beginning.\n", new_data);
+}
+
+
+void insertAtEnd(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *head_ref;
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    last->next = new_node;
+    printf("Inserted %d at the end.\n", new_data);
+}
+
+
+void deleteNode(struct Node** head_ref, int key) {
+    struct Node *temp = *head_ref, *prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next;
+        free(temp);
+        printf("Node with data %d deleted.\n", key);
+        return;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Value %d not found in the list.\n", key);
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+    printf("Node with data %d deleted.\n", key);
+}
+
+
+void traverse(struct Node* node) {
+    printf("Linked List: ");
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtBeginning(&head, 5);
+    traverse(head);
+
+    deleteNode(&head, 10);
+    traverse(head);
+
+    return 0;
+}
